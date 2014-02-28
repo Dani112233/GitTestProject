4820cdbe6d3cf4b664303525927e32c38eb485f1
WakkoTrakko <emhgithub@gmail.com>
2014-02-28 22:42:25 +0100

    This a first trial commiting changes.
    Remember initial commands :
    
    	Git add *
    	Git status
    	Git commit

---
+++ src/classes/Entity.java
@@ -0,0 +1,28 @@
+package classes;
+/**
+ * Entity class.
+ *
+ * @author anke
+ *
+ */
+public class Entity {
+    /**
+     * Number value.
+     */
+    private int number;
+    /**
+     * Gets number value.
+     *
+     * @return the value of number.
+     */
+    public final int getNumber() {
+        return number;
+    }
+    /**
+     * Sets number value.
+     * @param number number
+     */
+    public final void setNumber(final int number) {
+        this.number = number;
+    }
+}
---
+++ src/classes/package-info.java
@@ -0,0 +1,4 @@
+/**
+* Classes.
+*/
+package classes;
